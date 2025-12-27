6a4c43c4c580cef7415b701ab4ad9e251e57b67a
bhagyesh508 <bhagyeshyadav29@gmail.com>
2025-12-27 17:27:40 +0530

    successfully added crud operations with the help of primary key in function based views as well as class based views

--- project_drf/api/urls.py
+++ project_drf/api/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('students/',views.studentsview),
-    path('teachers/',views.teachersview)
+    path('teachers/',views.teachersview),
+    path('students/<int:pk>/',views.studentDetailview),
+    path('employees/',views.Employees.as_view()),
+    path('employees/<int:pk>/',views.EmployeeDetail.as_view()),
+    
     
 ]
